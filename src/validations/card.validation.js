8640bd2865545facfadd66258be0b09c96141a81
Nguyen Hai Dang <dang682000@gmail.com>
2022-05-22 23:17:20 +0700

    fix-eslint

--- src/validations/card.validation.js
+++ src/validations/card.validation.js
@@ -1,39 +1,39 @@
-import Joi from "joi";
-import { httpStatusCode } from "*/utils/constants";
+import Joi from 'joi'
+import { httpStatusCode } from '*/utils/constants'
 
 const createNew = async (req, res, next) => {
   const conditions = Joi.object({
     title: Joi.string().required().min(1),
     boardId: Joi.string().required().min(3).trim(),
     columnId: Joi.string().required().min(3).trim(),
-    cover: Joi.string(),
-  });
+    cover: Joi.string()
+  })
   try {
-    await conditions.validateAsync(req.body, { abortEarly: false });
-    next();
+    await conditions.validateAsync(req.body, { abortEarly: false })
+    next()
   } catch (e) {
     res
       .status(httpStatusCode.BAD_REQUEST)
-      .send({ error: new Error(e).message });
+      .send({ error: new Error(e).message })
   }
-};
+}
 
 const update = async (req, res, next) => {
   const conditions = Joi.object({
     boardId: Joi.string().required().min(3).trim(),
-    columnId: Joi.string().required().min(3).trim(),
-  });
+    columnId: Joi.string().required().min(3).trim()
+  })
   try {
     await conditions.validateAsync(req.body, {
       abortEarly: false,
-      allowUnknown: true,
-    });
-    next();
+      allowUnknown: true
+    })
+    next()
   } catch (e) {
     res
       .status(httpStatusCode.BAD_REQUEST)
-      .send({ error: new Error(e).message });
+      .send({ error: new Error(e).message })
   }
-};
+}
 
-export const CardValidations = { createNew, update };
+export const CardValidations = { createNew, update }
