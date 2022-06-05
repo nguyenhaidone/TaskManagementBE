3c3253edb63e1295863a125095c4e56c1a91b388
Nguyen Hai Dang <dang682000@gmail.com>
2022-06-05 22:10:50 +0700

    update-board-member

--- src/controllers/board.controller.js
+++ src/controllers/board.controller.js
@@ -1,114 +1,152 @@
-import { BoardServices } from '*/services/board.service'
-import { httpStatusCode } from '*/utils/constants'
-import { responseMessage } from '*/utils/common'
+import { BoardServices } from "*/services/board.service";
+import { httpStatusCode } from "*/utils/constants";
+import { responseMessage } from "*/utils/common";
 
 const createNew = async (req, res) => {
   try {
-    const result = await BoardServices.createNew(req.body)
+    const result = await BoardServices.createNew(req.body);
     res
       .status(httpStatusCode.OK)
-      .json(responseMessage(httpStatusCode.OK, result))
+      .json(responseMessage(httpStatusCode.OK, result));
   } catch (error) {
     res
       .status(httpStatusCode.INTERNAL_SERVER_ERROR)
-      .send({ error: error.message })
+      .send({ error: error.message });
   }
-}
+};
 
 const getFullBoard = async (req, res) => {
   try {
-    const { id } = await req.params
-    const result = await BoardServices.getFullBoard(id)
+    const { id } = await req.params;
+    const result = await BoardServices.getFullBoard(id);
     res
       .status(httpStatusCode.OK)
-      .json(responseMessage(httpStatusCode.OK, result))
+      .json(responseMessage(httpStatusCode.OK, result));
   } catch (error) {
     res
       .status(httpStatusCode.INTERNAL_SERVER_ERROR)
-      .send({ error: error.message })
+      .send({ error: error.message });
   }
-}
+};
 
 const update = async (req, res) => {
   try {
-    const { id } = await req.params
-    const result = await BoardServices.update(id, req.body)
+    const { id } = await req.params;
+    const result = await BoardServices.update(id, req.body);
     res
       .status(httpStatusCode.OK)
-      .json(responseMessage(httpStatusCode.OK, result))
+      .json(responseMessage(httpStatusCode.OK, result));
   } catch (error) {
     res
       .status(httpStatusCode.INTERNAL_SERVER_ERROR)
-      .send({ error: error.message })
+      .send({ error: error.message });
   }
-}
+};
 
 const getListBoardByUserIdController = async (req, res) => {
   try {
-    const accessTokenFromHeader = req.headers.x_authorization
+    const accessTokenFromHeader = req.headers.x_authorization;
     if (!accessTokenFromHeader) {
-      return res.status(400).send('Access token not exist.')
+      return res.status(400).send("Access token not exist.");
     }
     const result = await BoardServices.getListBoardByUserIdService(
       accessTokenFromHeader
-    )
+    );
     res
       .status(httpStatusCode.OK)
-      .json(responseMessage(httpStatusCode.OK, result))
+      .json(responseMessage(httpStatusCode.OK, result));
   } catch (error) {
     res
       .status(httpStatusCode.INTERNAL_SERVER_ERROR)
-      .send({ error: error.message })
+      .send({ error: error.message });
   }
-}
+};
 
 const addNewPeopleController = async (req, res) => {
   try {
-    const boardId = req.params
-    const email = req.body.email
-    const data = { userEmail: email, boardId: boardId.id }
-    const user = req.user
-    const result = await BoardServices.addNewPeopleService(user, data)
+    const boardId = req.params;
+    const email = req.body.email;
+    const data = { userEmail: email, boardId: boardId.id };
+    const user = req.user;
+    const result = await BoardServices.addNewPeopleService(user, data);
     res
       .status(httpStatusCode.OK)
-      .json(responseMessage(httpStatusCode.OK, result))
+      .json(responseMessage(httpStatusCode.OK, result));
   } catch (error) {
     res
       .status(httpStatusCode.INTERNAL_SERVER_ERROR)
-      .send({ error: error.message })
+      .send({ error: error.message });
   }
-}
+};
 
 const getListBoardJoinedOfCurrentUserController = async (req, res) => {
   try {
-    const curUser = req.user
+    const curUser = req.user;
     const result = await BoardServices.listBoardJoinedByCurrentUserService(
       curUser
-    )
+    );
     res
       .status(httpStatusCode.OK)
-      .json(responseMessage(httpStatusCode.OK, result))
+      .json(responseMessage(httpStatusCode.OK, result));
   } catch (error) {
     res
       .status(httpStatusCode.INTERNAL_SERVER_ERROR)
-      .send({ error: error.message })
+      .send({ error: error.message });
   }
-}
+};
 
 export const boardMessageController = async (req, res) => {
   try {
-    const boardId = req.params.id
-    const { message } = req.body
-    const result = await BoardServices.boardMessageService(boardId, message)
+    const boardId = req.params.id;
+    const { message } = req.body;
+    const result = await BoardServices.boardMessageService(boardId, message);
     res
       .status(httpStatusCode.OK)
-      .json(responseMessage(httpStatusCode.OK, result))
+      .json(responseMessage(httpStatusCode.OK, result));
   } catch (error) {
     res
       .status(httpStatusCode.INTERNAL_SERVER_ERROR)
-      .send({ error: error.message })
+      .send({ error: error.message });
   }
-}
+};
+
+export const removeCurrentUserController = async (req, res) => {
+  try {
+    const boardId = req.params.id;
+    console.log(boardId);
+    console.log(req.user);
+    const result = await BoardServices.removeCurrentUserService(
+      boardId,
+      req.user
+    );
+    res
+      .status(httpStatusCode.OK)
+      .json(responseMessage(httpStatusCode.OK, result));
+  } catch (error) {
+    res
+      .status(httpStatusCode.INTERNAL_SERVER_ERROR)
+      .send({ error: error.message });
+  }
+};
+
+export const removeMemberUserByCreateController = async (req, res) => {
+  try {
+    const boardId = req.params.id;
+    const { member } = req.body;
+    const result = await BoardServices.removeMemberUserByCreateService(
+      boardId,
+      req.user,
+      member
+    );
+    res
+      .status(httpStatusCode.OK)
+      .json(responseMessage(httpStatusCode.OK, result));
+  } catch (error) {
+    res
+      .status(httpStatusCode.INTERNAL_SERVER_ERROR)
+      .send({ error: error.message });
+  }
+};
 
 export const BoardController = {
   createNew,
@@ -117,5 +155,7 @@ export const BoardController = {
   getListBoardByUserIdController,
   addNewPeopleController,
   getListBoardJoinedOfCurrentUserController,
-  boardMessageController
-}
+  boardMessageController,
+  removeCurrentUserController,
+  removeMemberUserByCreateController,
+};
--- src/routes/v1/board.route.js
+++ src/routes/v1/board.route.js
@@ -1,29 +1,29 @@
-import express from 'express'
-import { BoardController } from '*/controllers/board.controller'
-import { BoardValidations } from '*/validations/board.validation'
-import { AuthMiddleware } from '*/middlewares/auth.middleware'
-import { BoardMiddleware } from '*/middlewares/board.middleware'
+import express from "express";
+import { BoardController } from "*/controllers/board.controller";
+import { BoardValidations } from "*/validations/board.validation";
+import { AuthMiddleware } from "*/middlewares/auth.middleware";
+import { BoardMiddleware } from "*/middlewares/board.middleware";
 
-const router = express.Router()
+const router = express.Router();
 
 router
-  .route('/')
+  .route("/")
   .get(AuthMiddleware.isAuth, BoardController.getListBoardByUserIdController)
   .post(
     AuthMiddleware.isAuth,
     BoardValidations.createNew,
     BoardController.createNew
-  )
+  );
 
 router
-  .route('/get-list-board-joined-by-current-user')
+  .route("/get-list-board-joined-by-current-user")
   .get(
     AuthMiddleware.isAuth,
     BoardController.getListBoardJoinedOfCurrentUserController
-  )
+  );
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(
     AuthMiddleware.isAuth,
     BoardMiddleware.isAccessBoard,
@@ -33,22 +33,38 @@ router
     AuthMiddleware.isAuth,
     BoardMiddleware.isAccessBoard,
     BoardController.update
-  )
+  );
 
 router
-  .route('/add-new-user/:id')
+  .route("/add-new-user/:id")
   .put(
     AuthMiddleware.isAuth,
     BoardMiddleware.isAccessBoard,
     BoardController.addNewPeopleController
-  )
+  );
 
 router
-  .route('/message/:id')
+  .route("/message/:id")
   .put(
     AuthMiddleware.isAuth,
     BoardMiddleware.isAccessBoard,
     BoardController.boardMessageController
-  )
+  );
 
-export const boardRoute = router
+router
+  .route("/remove-current-user/:id")
+  .put(
+    AuthMiddleware.isAuth,
+    BoardMiddleware.isAccessBoard,
+    BoardController.removeCurrentUserController
+  );
+
+router
+  .route("/remove-current-user-by-creater/:id")
+  .put(
+    AuthMiddleware.isAuth,
+    BoardMiddleware.isAccessBoard,
+    BoardController.removeMemberUserByCreateController
+  );
+
+export const boardRoute = router;
